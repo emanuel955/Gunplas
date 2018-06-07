b8fae7a791736e43d7b5f237dcb29705937f2142
Fran <Francomuzzio@hotmail.com>
2018-06-07 13:33:36 -0300

    Continuo trabajando con los equipos (Problema con las variables, ya envie mail para preguntar)

--- juego.py
+++ juego.py
@@ -4,11 +4,16 @@ def main():
     cant_pilotos = 1
     while es_primo(cant_pilotos):    #La cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
         cant_pilotos = randint(2,20)
-    print (cant_pilotos) #Prueba
+    print ("Cantidad de pilotos: ", cant_pilotos) #Prueba
     cant_equipos = randint(2, 5)
     while (cant_pilotos % cant_equipos != 0) or (cant_pilotos / cant_equipos > 5):   #Para que todos los equipos tengan la misma cantidad de jugadores y esta no sea mayor a 5
         cant_equipos = randint(2,5)
-    print (cant_equipos) #Prueba
+    print ("Cantidad de equipos: ", cant_equipos) #Prueba
+    equipos = []
+    for equipo in range(cant_equipos):    #Creo un listado con los equipos, dentro de cada equipo por ahora dejo la cantidad de integrantes que tiene solo para hacer pruebas
+        equipos.append([int(cant_pilotos / cant_equipos)])
+    print("Listado de equipos: ", equipos) #Prueba
+
 
 
 def es_primo(num):
@@ -16,3 +21,19 @@ def es_primo(num):
         if (num % i) == 0:
             return False
     return True
+
+main()
+print()
+main()
+print()
+main()
+print()
+main()
+print()
+main()
+print()
+main()
+print()
+main()
+print()
+
