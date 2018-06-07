721ae11016b589c645d844c47e3554352c3334f6
Fran <Francomuzzio@hotmail.com>
2018-06-07 13:01:46 -0300

    implementada asignacion de equipos

--- juego.py
+++ juego.py
@@ -1,10 +1,14 @@
 from random import randint
 
 def main():
-    pilotos = 1
-    while es_primo(pilotos):    #la cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
-        pilotos = randint(1,20)
-    print (pilotos)
+    cant_pilotos = 1
+    while es_primo(cant_pilotos):    #La cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
+        cant_pilotos = randint(2,20)
+    print (cant_pilotos) #Prueba
+    cant_equipos = randint(2, 5)
+    while (cant_pilotos % cant_equipos != 0) or (cant_pilotos / cant_equipos > 5):   #Para que todos los equipos tengan la misma cantidad de jugadores y esta no sea mayor a 5
+        cant_equipos = randint(2,5)
+    print (cant_equipos) #Prueba
 
 
 def es_primo(num):
@@ -12,5 +16,3 @@ def es_primo(num):
         if (num % i) == 0:
             return False
     return True
-
-main()
