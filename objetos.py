3668ba7e6807d0ccf315ef624b8d720f0fb19c72
Franco Muzzio <francomuzzio@hotmail.com>
2018-06-06 22:14:01 -0300

    Archivo de objetos que contiene las clases

---
+++ objetos.py
@@ -0,0 +1,49 @@
+class Gunpla:
+    def __init__(self, partes, armas):
+        self.partes = partes
+        self.armas = armas
+
+    def get_peso(self):
+        pass
+
+    def get_armadura(self):
+        pass
+
+    def get_velocidad(self):
+        pass
+
+    def get_energia(self):
+        pass
+
+    def get_energia_restante(self):
+        pass
+
+    def get_movilidad(self):
+        pass
+
+    def get_armamento(self):
+        pass
+
+
+class Esqueleto:
+    def __init__(self, energia, movilidad, max_armas):
+        self.energia = energia
+        self.movilidad = movilidad
+        self.max_armas = max_armas
+
+    def get_velocidad(self):
+        pass
+
+    def get_energia(self):
+        pass
+
+    def get_movilidad(self):
+        pass
+
+    def get_cantidaf_slots(self):
+        pass
+
+
+class Esqueleto:
+    def __init__(self, armas_adosadas, velocidad, armadura, escudo, energia):
+        self.
