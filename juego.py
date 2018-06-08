9b9e6432b197b090c5116760c1b4633bd51b0794
Fran <Francomuzzio@hotmail.com>
2018-06-07 23:24:53 -0300

    Muchos cambios
    -Completados (pero no terminado) los metodos faltantes de la clase Arma()
    
    -Mejorado el codigo y agregado el randomizer de armas

--- juego.py
+++ juego.py
@@ -1,18 +1,14 @@
-from random import randint
+from random import randint, choice
+from objetos import Gunpla, Esqueleto, Parte ,Arma
 
 def main():
-    cant_pilotos = 1
-    while es_primo(cant_pilotos):    #La cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
-        cant_pilotos = randint(2,20)
-    print ("Cantidad de pilotos: ", cant_pilotos) #Prueba
-    cant_equipos = randint(2, 5)
-    while (cant_pilotos % cant_equipos != 0) or (cant_pilotos / cant_equipos > 5):   #Para que todos los equipos tengan la misma cantidad de jugadores y esta no sea mayor a 5
-        cant_equipos = randint(2,5)
-    print ("Cantidad de equipos: ", cant_equipos) #Prueba
-    equipos = []
-    for equipo in range(cant_equipos):    #Creo un listado con los equipos, dentro de cada equipo por ahora dejo la cantidad de integrantes que tiene solo para hacer pruebas
-        equipos.append([int(cant_pilotos / cant_equipos)])
-    print("Listado de equipos: ", equipos) #Prueba
+    cant_pilotos, cant_equipos, cupo_equipo = aleatorizador_de_pilotos_y_equipos()
+    jugadores_equipos = repartir_pilotos(cant_equipos, cupo_equipo)
+    partes_y_armas = aleatorizador_de_partes_y_armas(cant_pilotos)
+
+
+
+
 
 
 
@@ -22,18 +18,62 @@ def es_primo(num):
             return False
     return True
 
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
+def aleatorizador_de_pilotos_y_equipos(): #La cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
+    cant_pilotos = randint(2,20)
+    cant_equipos = randint(2, 5)
+    cupo_equipo = int(cant_pilotos / cant_equipos)
+    while es_primo(cant_pilotos) or (cant_pilotos % cant_equipos != 0):
+        cant_pilotos = randint(2,20)
+        cant_equipos = randint(2, 5)
+        cupo_equipo = int(cant_pilotos / cant_equipos)
+    print ("Cantidad de pilotos: ", cant_pilotos) #Prueba
+    print ("Cantidad de equipos: ", cant_equipos) #Prueba
+    print ("Cupo por equipo: ", cupo_equipo) #Prueba
+    return cant_pilotos, cant_equipos, cupo_equipo
+
+def repartir_pilotos(cant_equipos, cupo_equipo): #Posiciona los pilotos en sus respectivos equipos
+    jugadores_equipos = []
+    for numero_de_equipo in range (cant_equipos):
+        for piloto_del_equipo in range (cupo_equipo):
+            jugadores_equipos.append(["Piloto " + str(piloto_del_equipo + 1), "Equipo " + str(numero_de_equipo + 1)]) #Lista de personaje = [Nombre, Equipo], es una lista para agregar mas cosas luego
+    print("Listado:\n", jugadores_equipos) #Prueba
+
+def aleatorizador_de_partes_y_armas(cant_pilotos):
+    armas = []
+    partes = []
+    while len(armas) < (cant_pilotos * 6):
+        armas.append(crear_arma())
+
+
+def crear_arma():
+    tipo_arma = ["MELEE", "RANGO"]
+    clases_melee = ["ESPADA", "DAGA", "MAZA", "HOZ", "PORRA", "BATE", "MARTILLO", "MOTOSIERRA", "KATANA", "LATIGO"]
+    clases_rango = ["BALLESTA", "ARCO", "PISTOLA", "RIFLE", "LANZA", "HONDA", "ESCOPETA", "BOOMERANG"]
+    municiones_arma = ["FISICA", "LASER", "HADRON"]
+    arma_peso = randint(0, 100)
+    arma_tipo = choice(tipo_arma)
+    if arma_tipo == "MELEE":
+        arma_clase = choice(clases_melee)
+    if arma_tipo == "RANGO":
+        arma_clase = choice(clases_rango)
+    arma_municion = choice(municiones_arma)
+    arma_velocidad = randint(-50,50)
+    arma_armadura = randint(-50,50)
+    arma_escudo = randint (-50,50)
+    arma_energia = randint (-50,50)
+    tiempo_de_recarga = randint (0,5)
+    arma_daño = randint (1,50)
+    arma_hits = randint (1,3)
+    arma_presicion = randint (0,4) #preguntar por valores de presicion
+    return Arma(arma_peso, arma_tipo, arma_clase, arma_municion, arma_velocidad, arma_armadura, arma_escudo, arma_energia, tiempo_de_recarga, arma_daño, arma_hits, arma_presicion)
+
+
+
+
+
+
+
+
 main()
 print()
 
--- objetos.py
+++ objetos.py
@@ -3,6 +3,7 @@ class Gunpla:
         self.partes = partes
         self.armas = armas
 
+
     def get_peso(self):
         pass
 
@@ -31,6 +32,7 @@ class Esqueleto:
         self.movilidad = movilidad
         self.max_armas = max_armas
 
+
     def get_velocidad(self):
         pass
 
@@ -40,7 +42,6 @@ class Esqueleto:
     def get_movilidad(self):
         pass
 
-<<<<<<< HEAD
     def get_cantidad_slots(self):
         pass
 
@@ -53,6 +54,7 @@ class Parte:
         self.armadura = armadura
         self.escudo = escudo
         self.energia = energia
+
 
     def get_peso(self):
         pass
@@ -77,43 +79,57 @@ class Parte:
 
 
 class Arma:
-    def __init__(self, peso, clase, tipo, municion, velocidad, armadura, escudo, energia, tiempo_recarga):
+    def __init__(self, peso, tipo, clase, municion, velocidad, armadura, escudo, energia, tiempo_recarga, daño, hits, presicion):
         self.peso = peso
-        self.clase = clase
         self.tipo = tipo
+        self.clase = clase
         self.municion = municion
         self.velocidad = velocidad
         self.armadura = armadura
         self.escudo = escudo
         self.energia = energia
+        self.daño = daño
+        self.hits = hits
+        self.presicion = presicion
         self.tiempo_recarga = tiempo_recarga
 
+
     def get_peso(self):
-        pass
+        return self.peso
+
+    def get_tipo(self):
+        return self.tipo
+
+    def get_clase(self):
+        return self.clase
+
+    def get_tipo_municion(self):
+        return self.municion
 
     def get_armadura(self):
-        pass
+        return self.armadura
 
     def get_escudo(self):
-        pass
+        return self.escudo
 
     def get_velocidad(self):
-        pass
+        return self.velocidad
 
     def get_energia(self):
-        pass
+        return self.energia
 
-    def get_armamento(self):
-        pass
+    def get_daño(self):
+        return self.daño
+
+    def get_hits(self):
+        return self.hits
+
+    def get_presicion(self):
+        return self.presicion
+
+    def esta_lista(self):
+        pass #AUN NO PUDEMOS HACER ESTA FUNCION
 
     def get_tipo_parte(self):
-        pass
-=======
-    def get_cantidaf_slots(self):
-        pass
+        return "ARMA"
 
-
-class Esqueleto:
-    def __init__(self, armas_adosadas, velocidad, armadura, escudo, energia):
-        self.
->>>>>>> 3668ba7e6807d0ccf315ef624b8d720f0fb19c72
