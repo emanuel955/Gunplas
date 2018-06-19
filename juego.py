9d401e5169f3689dcca9cec75dbd93022d4ff582
Fran <Francomuzzio@hotmail.com>
2018-06-19 20:00:06 -0300

    demasiado

--- juego.py
+++ juego.py
@@ -1,5 +1,6 @@
 from random import randint, choice, uniform
-from objetos import Gunpla, Esqueleto, Parte ,Arma, Pila, Cola
+from objetos import Gunpla, Esqueleto, Parte ,Arma, Pila, Cola, Piloto
+#from time import sleep
 
 def main():
     cant_pilotos, cant_equipos, cupo_equipo = aleatorizador_de_pilotos_y_equipos()
@@ -12,12 +13,11 @@ def main():
     jugadores_encolados = encolar_lista(jugadores_equipos)
     repartir_armas_partes(jugadores_encolados,partes_apiladas,armas_apiladas)
     elegir_partes(jugadores_encolados)
+    print(jugadores_encolados.desencolar())
     elegir_armas(jugadores_encolados)
-    gunplas_encolados = fabricar_gunpla(jugadores_encolados)
-    # encolar_por_velocidad(gunplas_encolados)
-    prueba = gunplas_encolados.desencolar()
-    # print (prueba)
-    print (prueba[2].get_movilidad())
+    gunplas_encolados = gunpla_por_velocidad(jugadores_encolados)
+    iniciar_batalla(gunplas_encolados, cant_equipos)
+
 
 
 def es_primo(num):
@@ -26,6 +26,54 @@ def es_primo(num):
         if (num % i) == 0:
             return False
     return True
+
+def quick_sort_velocidad(lista):
+    """Ordena la lista de forma recursiva.
+    Pre: los elementos de la lista deben ser comparables.
+    Post: la lista está ordenada. """
+    _quick_sort(lista, 0, len(lista) - 1)
+
+def _quick_sort(lista, inicio, fin):
+    """Función quick_sort recursiva.
+    Pre: los índices inicio y fin indican sobre qué porción operar.
+    Post: la lista está ordenada."""
+    if inicio >= fin:
+        return
+    menores = _partition(lista, inicio, fin)
+    _quick_sort(lista, inicio, menores - 1)
+    _quick_sort(lista, menores + 1, fin)
+
+def _partition(lista, inicio, fin):
+    """Función partición que trabaja sobre la misma lista.
+    Pre: los índices inicio y fin indican sobre qué porción operar.
+    Post: los menores están antes que el pivote, los mayores después.
+    Devuelve: la posición en la que quedó ubicado el pivote."""
+    pivote = lista[inicio][0].get_gunpla().get_velocidad()
+    menores = inicio
+    # Ubicar menores a la izquierda, mayores a la derecha
+    for i in range(inicio + 1, fin + 1):
+        if lista[i][0].get_gunpla().get_velocidad() < pivote:
+            menores += 1
+            if i != menores:
+                _swap(lista, i, menores)
+        # Ubicar el pivote al final de los menores
+    if inicio != menores:
+        _swap(lista, inicio, menores)
+    return menores
+
+def _swap(lista, i, j):
+    """Intercambia los elementos i y j de lista."""
+    lista[j], lista[i] = lista[i], lista[j]
+
+def enlistar_cola(cola):
+    '''recibe una cola y devuelve una lista con sus datos en el mismo orden'''
+    nueva_lista = []
+    while not cola.esta_vacia():
+        aux = cola.desencolar()
+        nueva_lista.append(aux)
+    for item in nueva_lista:
+        cola.encolar(item)
+    return nueva_lista
 
 def aleatorizador_de_pilotos_y_equipos(): #La cantidad de pilotos debe ser si o si un numero no primo para poder dividirlos en grupos iguales
     """Elige aleatoriamente una cantidad de pilotos (Maximo 20, minimo 2) y los divide en grupos iguales (maximo 5), luego, devuelve la cantidad de pilotos, equipos y el cupo de pilotos por equipo"""
@@ -46,7 +94,7 @@ def repartir_pilotos(cant_equipos, cupo_equipo):
     jugadores_equipos = []
     for numero_de_equipo in range (cant_equipos):
         for piloto_del_equipo in range (cupo_equipo):
-            jugadores_equipos.append(["Piloto " + str(piloto_del_equipo + 1), "Equipo " + str(numero_de_equipo + 1),[] ,[], [] ]) #Lista de personaje = [Nombre, Equipo, Esqueleto, Partes, Armas], es una lista para agregar mas cosas luego
+            jugadores_equipos.append([Piloto("Jugador " + str(piloto_del_equipo + 1)), "Equipo " + str(numero_de_equipo + 1),[] ,{"CASCO" : [],"PECHO" : [],"HOMBROS" : [],"PIERNAS" : [],"BOTAS" : [],"GUANTES" : [],"ALAS" : []}, [] ]) #Lista de personaje = [Nombre, Equipo, Esqueleto, Partes, Armas], es una lista para agregar mas cosas luego
     jugadores_equipos_mezclados = mezclar_lista(jugadores_equipos)
     return jugadores_equipos_mezclados
 
@@ -63,9 +111,9 @@ def aleatorizador_de_partes_y_armas(cant_pilotos):
     """Recibe la cantidad de pilotos y, dependiendo de esta, crea una cantidad determinada de armas y partes para que estos tengan varias para elegir, los devuelve en dos listas de Objetos"""
     armas = []
     partes = []
-    while len(armas) < (cant_pilotos * 6): #2 tipos de armas * 3 * pilotos (para que tengan para elegir)
+    while len(armas) < (cant_pilotos * 14):
         armas.append(crear_arma())
-    while len(partes) < (cant_pilotos * 21): #7 tipos de partes * 3 * pilotos (para que tengan para elegir)
+    while len(partes) < (cant_pilotos * 42):
         partes.append(crear_parte())
     return partes, armas
 
@@ -87,59 +135,59 @@ def crear_arma():
     arma_escudo = randint (-50,50)
     arma_energia = randint (-100,300)
     tiempo_de_recarga = randint (0,5)
-    arma_daño = randint (1,50)
+    arma_dano = randint (1,50)
     arma_hits = randint (1,3)
     arma_presicion = round(uniform (0.0, 4.0),2) #Round redondea a dos decimales
-    return Arma(arma_peso, arma_tipo, arma_clase, arma_municion, arma_velocidad, arma_armadura, arma_escudo, arma_energia, tiempo_de_recarga, arma_daño, arma_hits, arma_presicion)
+    return Arma(arma_peso, arma_tipo, arma_clase, arma_municion, arma_velocidad, arma_armadura, arma_escudo, arma_energia, tiempo_de_recarga, arma_dano, arma_hits, arma_presicion)
 
 def crear_parte():
     """Crea una parte aleatoria y la devuelve como objeto"""
     parte_tipo = choice(["CASCO","PECHO","HOMBROS","PIERNAS","BOTAS","GUANTES","ALAS"])
     if parte_tipo == "CASCO":
         parte_peso = randint(0, 75)
-        parte_velocidad = randint(-20, 0)
+        parte_velocidad = randint(-35, -10)
         parte_slots_armas = randint (0,1)
         parte_armadura = randint(-20, 40)
         parte_escudo = randint(-20, 40)
         parte_energia = randint(-100, 300)
     if parte_tipo == "PECHO":
         parte_peso = randint(0, 110)
-        parte_velocidad = randint(-50, 0)
+        parte_velocidad = randint(-600, -10)
         parte_slots_armas = 0
-        parte_armadura = randint(-20, 50)
+        parte_armadura = randint(-30, 50)
         parte_escudo = randint(-20, 50)
         parte_energia = randint(-100,400)
     if parte_tipo == "HOMBROS":
         parte_peso = randint(0, 30)
-        parte_velocidad = randint(-50, 0)
+        parte_velocidad = randint(-50, -5)
         parte_slots_armas = 0
         parte_armadura = randint(-20, 30)
         parte_escudo = randint(-20, 50)
         parte_energia = randint(-100, 150)
     if parte_tipo == "PIERNAS":
         parte_peso = randint(0, 90)
-        parte_velocidad = randint(-50, 60)
+        parte_velocidad = randint(-80, 60)
         parte_slots_armas = 0
         parte_armadura = randint(-30, 60)
         parte_escudo = randint(-20, 50)
         parte_energia = randint(-100,350)
     if parte_tipo == "BOTAS":
         parte_peso = randint(0, 50)
-        parte_velocidad = randint(-30, 60)
+        parte_velocidad = randint(-70, 40)
         parte_slots_armas = 0
         parte_armadura = randint(-30, 25)
         parte_escudo = randint(-20, 20)
         parte_energia = randint(-100,200)
     if parte_tipo == "GUANTES":
         parte_peso = randint(0, 20)
-        parte_velocidad = randint(-30, 20)
+        parte_velocidad = randint(-30, 5)
         parte_slots_armas = randint(0,1)
         parte_armadura = randint(-10, 10)
         parte_escudo = randint(-10, 10)
         parte_energia = randint(-100,150)
     if parte_tipo == "ALAS":
         parte_peso = randint(0, 75)
-        parte_velocidad = randint(-50, 50)
+        parte_velocidad = randint(-30, 70)
         parte_slots_armas = randint(0,2)
         parte_armadura = randint(-25, 25)
         parte_escudo = randint(-60, 50)
@@ -157,7 +205,7 @@ def aleatorizador_de_esqueletos(cant_pilotos):
     esqueletos = []
     for i in range (cant_pilotos * 3):
         esqueleto_energia = randint (1, 1000)
-        esqueleto_movilidad = randint (100, 300)
+        esqueleto_movilidad = randint (100, 400)
         esqueleto_velocidad = randint (200,400)
         esqueleto_slot_armas = randint (0, 3)
         esqueletos.append(Esqueleto(esqueleto_energia,esqueleto_movilidad, esqueleto_slot_armas, esqueleto_velocidad))
@@ -173,7 +221,7 @@ def apilar_lista(lista):
 def elegir_esqueletos(jugadores, esqueletos):
     """Recibe la lista de jugadores y la de esqueletos y hace que estos los elijan aleatoriamente"""
     for jugador in jugadores:
-        jugador[2] = (choice(esqueletos))
+        jugador[2] = jugador[0].elegir_esqueleto(esqueletos)
 
 def encolar_lista(lista):
     """Recibe una lista y la devuelve encolada"""
@@ -183,38 +231,32 @@ def encolar_lista(lista):
     return nueva_cola
 
 def repartir_armas_partes(jugadores_encolados,partes_apiladas,armas_apiladas):
-    """Recibe la cola de jugadores, la lista de partes y la de armas, luego hace que estos en su turno recojan aleatoriamente (la agreguen a la lista de personajes y sus caracteristicas) un arma o parte que este en el tope de su respectiva fila, los jugadores se vuelven a encolar hasta que ambas listas queden vacias"""
+    """Recibe la cola de jugadores, la lista de partes y la de armas, luego hace que estos en su turno recojan aleatoriamente (la agreguen a la lista de personajes y sus caracteristicas) un arma o parte que este en el tope de su respectiva fila, los jugadores se vuelven a encolar hasta que ambas listas queden vacias. Cada jugador debe tener al meno un arma"""
     while not partes_apiladas.esta_vacia() or not armas_apiladas.esta_vacia():
         jugador = jugadores_encolados.desencolar()
-        if partes_apiladas.esta_vacia():
+        if partes_apiladas.esta_vacia() or len(jugador[4]) == 0:
             objeto_a_elegir = "ARMAS"
         elif armas_apiladas.esta_vacia():
             objeto_a_elegir = "PARTES"
         else:
             objeto_a_elegir = choice(["ARMAS", "PARTES"])
         if objeto_a_elegir == "PARTES":
-            jugador[3].append(partes_apiladas.desapilar())
+            parte_elegida = partes_apiladas.desapilar()
+            jugador[3][parte_elegida.get_tipo_parte()].append(parte_elegida)
+            # print(str(jugador[0])+" DEL "+jugador[1]+" HA ELEGIDO UNA PARTE")
         if objeto_a_elegir == "ARMAS":
             jugador[4].append(armas_apiladas.desapilar())
+            # print(str(jugador[0])+" DEL "+jugador[1]+" HA ELEGIDO UN ARMA")
         jugadores_encolados.encolar(jugador)
+    # sleep(0.1)
 
 def elegir_partes(jugadores_encolados):
     """Recibe la cola de jugadores y a cada uno lo hace quedarse con una parte de cada tipo, si es que cuentan con todos los tipos de armas, y las demas son desechadas"""
     cola_aux = Cola()
     while not jugadores_encolados.esta_vacia():
-        lista_aux = []
-        lista_aux_tipos = []
         jugador = jugadores_encolados.desencolar()
         cola_aux.encolar(jugador)
-        while (not "CASCO" in lista_aux) or (not "PECHO" in lista_aux) or (not "HOMBROS" in lista_aux) or (not "PIERNAS" in lista_aux) or (not "BOTAS" in lista_aux) or (not "GUANTES" in lista_aux) or (not "ALAS" in lista_aux):
-            aux = choice(jugador[3])
-            jugador[3].remove(aux)
-            if aux.get_tipo_parte() not in lista_aux_tipos:
-                lista_aux_tipos.append(aux.get_tipo_parte())
-                lista_aux.append(aux)
-            if not jugador[3]:
-                break
-        jugador[3] = lista_aux
+        jugador[3] = jugador[0].elegir_combinacion(jugador[3])
     while not cola_aux.esta_vacia():
         jugadores_encolados.encolar(cola_aux.desencolar())
 
@@ -226,6 +268,8 @@ def elegir_armas(jugadores_encolados):
         jugador = jugadores_encolados.desencolar()
         cola_aux.encolar(jugador)
         for i in range (jugador[2].get_cantidad_slots()):
+            if len(jugador[4]) == 0:
+                break
             aux = choice(jugador[4])
             jugador[4].remove(aux)
             lista_aux.append(aux)
@@ -233,14 +277,44 @@ def elegir_armas(jugadores_encolados):
     while not cola_aux.esta_vacia():
         jugadores_encolados.encolar(cola_aux.desencolar())
 
-def fabricar_gunpla(jugadores_encolados):
+def gunpla_por_velocidad(jugadores_encolados):
+    '''Recibe la cola de jugadores, fabrica sus gumplas y devuelve una nueva cola con los jugadores encolados por velocidad (mayor a menor)'''
     nueva_cola = Cola()
+    nueva_lista = []
     while not jugadores_encolados.esta_vacia():
         jugador = jugadores_encolados.desencolar()
-        gunpla_fabricado = Gunpla(jugador[2],jugador[3],jugador[4])
-        nueva_cola.encolar([jugador[0],jugador[1],gunpla_fabricado])
+        jugador[0].set_gunpla(Gunpla(jugador[2],jugador[3],jugador[4]))
+        nueva_lista.append([jugador[0],jugador[1]])
+    quick_sort_velocidad(nueva_lista)
+    nueva_lista = list(reversed(nueva_lista))
+    for piloto_gunpla in nueva_lista:
+        nueva_cola.encolar(piloto_gunpla)
     return nueva_cola
 
+def iniciar_batalla(gunplas_encolados, cant_equipos):
+    cola_jugadores_enlistada = enlistar_cola(gunplas_encolados)
+    equipos = []
+    for i in range(cant_equipos):
+        equipos.append([])
+    for jugador in cola_jugadores_enlistada:
+        n_equipo = jugador[1].split(" ")[1]
+        equipos[int(n_equipo)-1].append(jugador)
+    while len(equipos) > 1:
+        jugador_atacante = gunplas_encolados.desencolar()
+        jugador_atacado = elegir_objetivo(jugador_atacante, equipos)
+        arma_elegida = jugador_atacante[0].elegir_arma(jugador_atacante[0].get_gunpla().get_armamento())
+        atacar(jugador_atacante,jugador_atacado)
+
+def elegir_objetivo(atacante, equipos):
+    '''recibe el atacante y la lista de equipos y elije y devuelve un objetivo de otro equipo'''
+    enemigos = []
+    for equipo in equipos:
+        for jugador in equipo:
+            if jugador != atacante and jugador[1] != atacante[1]:
+                enemigos.append(jugador)
+    return atacante[0].elegir_oponente(enemigos)
+
+def atacar(atacante, victima, arma_elegida):
 
 
 
@@ -248,52 +322,8 @@ def fabricar_gunpla(jugadores_encolados):
 
 
 
+
 main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
-main()
-print()
+
+
 
--- objetos.py
+++ objetos.py
@@ -1,130 +1,4 @@
-class Gunpla:
-    def __init__(self, esqueleto, partes = None, armas = None):
-        self.esqueleto = esqueleto
-        self.partes = partes
-        self.armas = armas
-
-    def __repr__(self):
-        return "["+str(self.esqueleto)+" ,"+str(self.partes)+" ,"+str(self.armas)+"]"
-
-
-    def get_peso(self):
-        contador = 0
-        for parte in self.partes:
-            if parte.get_armamento() != 0:
-                lista_aux = parte.get_armamento()
-                for arma in lista_aux:
-                    contador += arma.get_peso()
-            contador += parte.get_peso()
-        for arma in self.armas:
-            contador += arma.get_peso()
-        return contador
-
-
-    def get_armadura(self):
-        contador = 0
-        for parte in self.partes:
-            if parte.get_armamento() != 0:
-                lista_aux = parte.get_armamento()
-                for arma in lista_aux:
-                    contador += arma.get_armadura()
-            contador += parte.get_armadura()
-        for arma in self.armas:
-            contador += arma.get_armadura()
-        return contador
-
-    def get_velocidad(self):
-        contador = 0
-        for parte in self.partes:
-            if parte.get_armamento() != 0:
-                lista_aux = parte.get_armamento()
-                for arma in lista_aux:
-                    contador += arma.get_velocidad()
-            contador += parte.get_velocidad()
-        for arma in self.armas:
-            contador += arma.get_velocidad()
-        contador += self.esqueleto.get_velocidad()
-        return contador
-
-    def get_energia(self):
-        contador = 0
-        for parte in self.partes:
-            if parte.get_armamento() != 0:
-                lista_aux = parte.get_armamento()
-                for arma in lista_aux:
-                    contador += arma.get_energia()
-            contador += parte.get_energia()
-        for arma in self.armas:
-            contador += arma.get_energia()
-        contador += self.esqueleto.get_energia()
-        return contador
-
-    def get_energia_restante(self):
-        pass
-
-    def get_movilidad(self):
-        base = self.esqueleto.get_movilidad()
-        return (base - self.get_peso() / 2 + self.get_velocidad() * 3) / base
-
-    def get_armamento(self):
-        pass
-
-class Esqueleto:
-    def __init__(self, energia, movilidad, max_armas, velocidad):
-        self.energia = energia
-        self.movilidad = movilidad
-        self.max_armas = max_armas
-        self.velocidad = velocidad
-
-
-    def get_velocidad(self):
-        return self.velocidad
-
-    def get_energia(self):
-        return self.energia
-
-    def get_movilidad(self):
-        return self.movilidad
-
-    def get_cantidad_slots(self):
-        return self.max_armas
-
-    def __repr__(self):
-        return "["+str(self.velocidad)+"Km/h, Movilidad:"+str(self.movilidad)+", Energ:"+str(self.energia)+", Max Armas:"+str(self.max_armas)+"]"
-
-class Parte:
-    def __init__(self, peso, armas_adosadas, velocidad, armadura, escudo, energia, tipo):
-        self.peso = peso
-        self.armas_adosadas = armas_adosadas
-        self.velocidad = velocidad
-        self.armadura = armadura
-        self.escudo = escudo
-        self.energia = energia
-        self.tipo = tipo
-
-    def __repr__(self):
-        return "["+str(self.peso)+"kg, Tipo:"+self.tipo+", "+str(self.velocidad)+"Km/h, Armor:"+str(self.armadura)+", Escu:"+str(self.escudo)+", Energ:"+str(self.energia)+", Armamento:"+str(self.armas_adosadas)+"]"
-
-    def get_peso(self):
-        return self.peso
-
-    def get_velocidad(self):
-        return self.velocidad
-
-    def get_armadura(self):
-        return self.armadura
-
-    def get_escudo(self):
-        return self.escudo
-
-    def get_energia(self):
-        return self.energia
-
-    def get_armamento(self):
-        return self.armas_adosadas
-
-    def get_tipo_parte(self):
-        return self.tipo
+from random import choice
 
 class Arma:
     def __init__(self, peso, tipo, clase, municion, velocidad, armadura, escudo, energia, tiempo_recarga, dano, hits, presicion):
@@ -183,6 +57,182 @@ class Arma:
 
     def get_tipo_parte(self):
         return "ARMA"
+
+class Gunpla:
+    def __init__(self, esqueleto, partes = None, armas = [Arma(1, "SPOON", "MELEE", "FISICA", 1, 0, 0, 0, 0, 1, 10, 0.10)]):
+        self.esqueleto = esqueleto
+        self.partes = partes
+        self.armas = armas
+
+    def __repr__(self):
+        return "["+str(self.esqueleto)+" ,"+str(self.partes)+" ,"+str(self.armas)+"]"
+
+
+    def get_peso(self):
+        contador = 0
+        for parte in self.partes:
+            if parte.get_armamento() != 0:
+                lista_aux = parte.get_armamento()
+                for arma in lista_aux:
+                    contador += arma.get_peso()
+            contador += parte.get_peso()
+        for arma in self.armas:
+            contador += arma.get_peso()
+        return contador
+
+    def get_armadura(self):
+        contador = 0
+        for parte in self.partes:
+            if parte.get_armamento() != 0:
+                lista_aux = parte.get_armamento()
+                for arma in lista_aux:
+                    contador += arma.get_armadura()
+            contador += parte.get_armadura()
+        for arma in self.armas:
+            contador += arma.get_armadura()
+        return contador
+
+    def get_velocidad(self):
+        contador = 0
+        for parte in self.partes:
+            if parte.get_armamento() != 0:
+                lista_aux = parte.get_armamento()
+                for arma in lista_aux:
+                    contador += arma.get_velocidad()
+            contador += parte.get_velocidad()
+        for arma in self.armas:
+            contador += arma.get_velocidad()
+        contador += self.esqueleto.get_velocidad()
+        return contador
+
+    def get_energia(self):
+        contador = 0
+        for parte in self.partes:
+            if parte.get_armamento() != 0:
+                lista_aux = parte.get_armamento()
+                for arma in lista_aux:
+                    contador += arma.get_energia()
+            contador += parte.get_energia()
+        for arma in self.armas:
+            contador += arma.get_energia()
+        contador += self.esqueleto.get_energia()
+        return contador
+
+    def get_energia_restante(self):
+        pass
+
+    def get_movilidad(self):
+        base = self.esqueleto.get_movilidad()
+        return (base - self.get_peso() / 2 + self.get_velocidad() * 3) / base
+
+    def get_armamento(self):
+        total_armas = []
+        for parte in self.partes:
+            if parte.get_armamento() != 0:
+                for arma_en_parte in parte.get_armamento():
+                    total_armas.append(arma_en_parte)
+        for arma_de_gunpla in self.armas:
+            total_armas.append(arma_de_gunpla)
+        return total_armas
+
+class Esqueleto:
+    def __init__(self, energia, movilidad, max_armas, velocidad):
+        self.energia = energia
+        self.movilidad = movilidad
+        self.max_armas = max_armas
+        self.velocidad = velocidad
+
+
+    def get_velocidad(self):
+        return self.velocidad
+
+    def get_energia(self):
+        return self.energia
+
+    def get_movilidad(self):
+        return self.movilidad
+
+    def get_cantidad_slots(self):
+        return self.max_armas
+
+    def __repr__(self):
+        return "["+str(self.velocidad)+"Km/h, Movilidad:"+str(self.movilidad)+", Energ:"+str(self.energia)+", Max Armas:"+str(self.max_armas)+"]"
+
+class Parte:
+    def __init__(self, peso, armas_adosadas, velocidad, armadura, escudo, energia, tipo):
+        self.peso = peso
+        self.armas_adosadas = armas_adosadas
+        self.velocidad = velocidad
+        self.armadura = armadura
+        self.escudo = escudo
+        self.energia = energia
+        self.tipo = tipo
+
+    def __repr__(self):
+        return "["+str(self.peso)+"kg, Tipo:"+self.tipo+", "+str(self.velocidad)+"Km/h, Armor:"+str(self.armadura)+", Escu:"+str(self.escudo)+", Energ:"+str(self.energia)+", Armamento:"+str(self.armas_adosadas)+"]"
+
+    def get_peso(self):
+        return self.peso
+
+    def get_velocidad(self):
+        return self.velocidad
+
+    def get_armadura(self):
+        return self.armadura
+
+    def get_escudo(self):
+        return self.escudo
+
+    def get_energia(self):
+        return self.energia
+
+    def get_armamento(self):
+        return self.armas_adosadas
+
+    def get_tipo_parte(self):
+        return self.tipo
+
+class Piloto:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.gunpla = None
+
+    def __repr__(self):
+        return self.nombre
+
+    def set_gunpla(self,gunpla):
+        self.gunpla = gunpla
+
+    def get_gunpla(self):
+        return self.gunpla
+
+    def elegir_esqueleto(self, lista_esqueletos):
+        esqueleto_elegido = choice(lista_esqueletos)
+        return esqueleto_elegido
+
+    def elegir_parte(self, partes_disponibles):
+        tipos = list(partes_disponibles.keys())
+        return choice(tipos)
+
+    def elegir_combinacion(self, partes_reservadas):
+        tipo_y_parte = {}
+        while ("CASCO" not in tipo_y_parte) or ("PECHO" not in tipo_y_parte) or ("HOMBROS" not in tipo_y_parte) or ("PIERNAS" not in tipo_y_parte) or ("BOTAS" not in tipo_y_parte) or ("GUANTES" not in tipo_y_parte) or ("ALAS" not in tipo_y_parte) :
+            tipo_elegido = self.elegir_parte(partes_reservadas)
+            if (tipo_elegido not in tipo_y_parte) and (tipo_elegido in partes_reservadas) and (len(partes_reservadas[tipo_elegido]) != 0):
+                tipo_y_parte[tipo_elegido] = choice(partes_reservadas[tipo_elegido])
+                del partes_reservadas[tipo_elegido]
+            elif (len(partes_reservadas[tipo_elegido]) == 0):
+                del partes_reservadas[tipo_elegido]
+            if not partes_reservadas:
+                partes_reservadas = list(tipo_y_parte.values())
+                break
+        return partes_reservadas
+
+    def elegir_oponente(self, oponentes):
+        return choice(oponentes)
+
+    def elegir_arma(self, armamento):
+        return choice(armamento)
 
 class Nodo:
     def __init__(self, dato = None, prox = None):
@@ -268,6 +318,7 @@ class Cola:
 
     def __str__(self):
         return str(self.lista.len) + " elementos"
+        # return str(self.lista.prim.dato)
 
     def encolar(self,dato):
             self.lista.insert(dato)
